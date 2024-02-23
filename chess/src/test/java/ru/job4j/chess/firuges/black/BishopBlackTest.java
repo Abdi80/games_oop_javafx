76c53404ffe22860c0b7a36c1e5c8c381b54c9b1
Nurlan <001nurlan@gmail.com>
2024-02-23 21:36:52 +0600

    3. Каркас шахматной доски [#792 #482856]

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -3,8 +3,6 @@ package ru.job4j.chess.firuges.black;
 import org.junit.jupiter.api.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
