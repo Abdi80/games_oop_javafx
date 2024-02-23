bbedf033b5802adfea5c631d0f1c9a41913bfe89
Nurlan <001nurlan@gmail.com>
2024-02-23 20:42:28 +0600

    3. Каркас шахматной доски [#792]

---
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -0,0 +1,51 @@
+package ru.job4j.chess.firuges.black;
+
+import ru.job4j.chess.ImpossibleMoveException;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+public class BishopBlack implements Figure {
+    private final Cell position;
+
+    public BishopBlack(final Cell ps) {
+        position = ps;
+    }
+
+    @Override
+    public Cell position() {
+        return position;
+    }
+
+    @Override
+    public Cell[] way(Cell dest) {
+        if (!isDiagonal(position, dest)) {
+            throw new ImpossibleMoveException(
+                    String.format("Could not way by diagonal from %s to %s", position, dest)
+            );
+        }
+        int startX = position.getX();
+        int startY = position.getY();
+        int finishX = dest.getX();
+        int size = Math.max(startX, finishX) - Math.min(startX, finishX);
+        Cell[] steps = new Cell[size];
+        int deltaX = startX <= finishX ? 1 : -1;
+        int deltaY = startY <= dest.getY() ? 1 : -1;
+        for (int index = 0; index < size; index++) {
+            steps[index] = Cell.findBy(startX + (index + 1) * deltaX, startY + (index + 1) * deltaY);
+        }
+        return steps;
+    }
+
+    public boolean isDiagonal(Cell source, Cell dest) {
+        int maxX = Math.max(source.getX(), dest.getX());
+        int minX = Math.min(source.getX(), dest.getX());
+        int maxY = Math.max(source.getY(), dest.getY());
+        int minY = Math.min(source.getY(), dest.getY());
+        return (maxX - minX) == (maxY - minY) ? true : false;
+    }
+
+    @Override
+    public Figure copy(Cell dest) {
+        return new BishopBlack(dest);
+    }
+}
---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,64 @@
+package ru.job4j.chess.firuges.black;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class BishopBlackTest {
+
+    @Test
+    void whenPositionC8() {
+        Cell cell = Cell.C1;
+        BishopBlack bishopBlack = new BishopBlack(cell);
+        assertThat(bishopBlack.position()).isEqualTo(cell);
+    }
+
+    @Test
+    void whenCopy() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        Cell newPosition = Cell.A3;
+        Figure copyBishopBlack = bishopBlack.copy(newPosition);
+        assertThat(copyBishopBlack.position()).isEqualTo(newPosition);
+
+    }
+
+    @Test
+    void whenWayC1G5() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        Cell[] way = bishopBlack.way(Cell.G5);
+        Cell[] expected = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        assertThat(way).isEqualTo(expected);
+    }
+
+    @Test
+    void whenWayG5C1() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.G5);
+        Cell[] way = bishopBlack.way(Cell.C1);
+        Cell[] expected = {Cell.F4, Cell.E3, Cell.D2, Cell.C1};
+        assertThat(way).isEqualTo(expected);
+    }
+
+    @Test
+    void whenWayF4D6() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.F4);
+        Cell[] way = bishopBlack.way(Cell.D6);
+        Cell[] expected = {Cell.E5, Cell.D6};
+        assertThat(way).isEqualTo(expected);
+    }
+
+    @Test
+    void whenWayB5F1() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.B5);
+        Cell[] way = bishopBlack.way(Cell.F1);
+        Cell[] expected = {Cell.C4, Cell.D3, Cell.E2, Cell.F1};
+        assertThat(way).isEqualTo(expected);
+    }
+
+
+
+
+}
